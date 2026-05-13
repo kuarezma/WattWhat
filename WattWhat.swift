e58a884f0a37c0ac94a5d72ccb0e133913fcfb82
Uğur <yasayanugur@gmail.com>
2026-05-13 20:09:17 +0300

    UI: Re-enable items using dummyAction to fix macOS disabled color replacement

--- WattWhat.swift
+++ WattWhat.swift
@@ -26,12 +26,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         
         let menu = NSMenu()
-        averageWattageItem = NSMenuItem(title: "Ortalama: -- W", action: nil, keyEquivalent: "")
-        averageWattageItem.isEnabled = false
+        averageWattageItem = NSMenuItem(title: "Ortalama: -- W", action: #selector(dummyAction), keyEquivalent: "")
+        averageWattageItem.target = self
         menu.addItem(averageWattageItem)
         
-        temperatureItem = NSMenuItem(title: "Pil Sıcaklığı: -- °C", action: nil, keyEquivalent: "")
-        temperatureItem.isEnabled = false
+        temperatureItem = NSMenuItem(title: "Pil Sıcaklığı: -- °C", action: #selector(dummyAction), keyEquivalent: "")
+        temperatureItem.target = self
         menu.addItem(temperatureItem)
         
         menu.addItem(NSMenuItem.separator())
@@ -77,6 +77,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
+    @objc func dummyAction() {}
+    
     @objc func closeAllApps() {
         let apps = NSWorkspace.shared.runningApplications
         let currentApp = NSRunningApplication.current
@@ -198,7 +200,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 
                 let attrStr = NSMutableAttributedString(string: tempPrefix, attributes: [
                     .font: menuFont,
-                    .foregroundColor: NSColor(calibratedWhite: 0.999, alpha: 1.0)
+                    .foregroundColor: NSColor.white
                 ])
                 let valAttrStr = NSAttributedString(string: tempValue, attributes: [
                     .font: menuFont,
@@ -260,7 +262,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         let avgAttrStr = NSMutableAttributedString(string: avgPrefix, attributes: [
             .font: menuFont,
-            .foregroundColor: NSColor(calibratedWhite: 0.999, alpha: 1.0)
+            .foregroundColor: NSColor.white
         ])
         let avgValAttrStr = NSAttributedString(string: avgValue, attributes: [
             .font: menuFont,
